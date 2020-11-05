eaf135fe2d2f2990426eb7d4bda11ae2f6735dbc
James Woodley <james@lightestnight.com>
2020-11-05 18:32:34 +0000

    Add IHost Extensions

---
+++ AspNetCore/ExtendsHost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using LightestNight.System.EventSourcing.Events;
+using Microsoft.Extensions.Hosting;
+
+namespace LightestNight.System.EventSourcing.AspNetCore
+{
+    public static class ExtendsHost
+    {
+        public static IHost UseEventSourcing(this IHost host, params Assembly[] assemblies)
+        {
+            EventCollection.AddAssemblyTypes(assemblies);
+            return host;
+        }
+
+        public static IHost UseEventSourcing(this IHost host, params Type[] pointerTypes)
+        {
+            EventCollection.AddAssemblyTypes(pointerTypes.Select(type => type.Assembly).ToArray());
+            return host;
+        }
+    }
+}
